5674f8d24e6ae9026709f8475d32cb825b95b72d
Sunrisepeak <speakshen@163.com>
2021-01-07 14:27:36 +0800

    plot-scatter&continuous

---
+++ data_visualization/ch15_generating_data/mpl_squares.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+
+input_values = list(range(0, 6));
+squares = [v**2 for v in range(0, 6)];
+plt.plot(input_values, squares, linewidth = 5);
+
+plt.title("Square Numbers", fontsize = 16);
+plt.xlabel("Value", fontsize = 10);
+plt.ylabel("SValue", fontsize = 10);
+
+plt.tick_params(axis = 'both', labelsize = 14);
+
+plt.show();
